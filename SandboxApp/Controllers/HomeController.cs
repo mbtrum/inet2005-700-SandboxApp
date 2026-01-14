205b4aba2ef8b1ce9611a27651e94e4f6400dc65
Michael Trumbull <michael.trumbull@nscc.ca>
2026-01-14 10:26:19 -0400

    Learning controllers and routing.

--- SandboxApp/Controllers/HomeController.cs
+++ SandboxApp/Controllers/HomeController.cs
@@ -6,20 +6,53 @@ namespace SandboxApp.Controllers
 {
     public class HomeController : Controller
     {
+        // Constructor
+        public HomeController()
+        {
+        }
+
         public IActionResult Index()
         {
+            // system automatically looks in View/Home/ for Index.cshtml
             return View();
         }
 
         public IActionResult Privacy()
         {
-            return View();
+            // we can spell out the view to return
+            return View("Privacy");
         }
+
+        public ViewResult HelloWorld()
+        {
+            // we can return any view
+            return View("Privacy");
+        }
+
+        public ContentResult PlainText()
+        {
+            return Content("Hello, this is plain text!");
+        }
+
+        public RedirectResult GoodbyeWorld()
+        {
+            return Redirect("https://nscc.ca");
+        }
+        public UnauthorizedResult SecureWebpage()
+        {
+            return new UnauthorizedResult();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private void Test()
+        {
+
         }
     }
 }
