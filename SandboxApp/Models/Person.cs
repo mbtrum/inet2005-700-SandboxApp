a7a99ad09926110341f5696a326c904fd760beb4
Michael Trumbull <michael.trumbull@nscc.ca>
2026-01-22 11:49:27 -0400

    Create an run first migration - create Photo and Person DB tables.

--- SandboxApp/Models/Person.cs
+++ SandboxApp/Models/Person.cs
@@ -2,6 +2,7 @@
 {
     public class Person
     {
+        // unique ID
         public int Id { get; set; }
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
